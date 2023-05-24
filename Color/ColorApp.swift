acba07607a3ed0fb396d826069ddb8806930f9f0
aamoskekkonen <aamoskekkonen@icloud.com>
2023-05-24 09:39:30 +0300

    Initial Commit

---
+++ Color/ColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorApp.swift
+//  Color
+//
+//  Created by Aamos Kekkonen on 24.5.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
