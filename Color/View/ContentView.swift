86330c3446170336b3f16cf0627e95b0732b7ed0
aamoskekkonen <aamoskekkonen@icloud.com>
2023-06-01 21:40:42 +0300

    add Canvas to ContentView

--- Color/View/ContentView.swift
+++ Color/View/ContentView.swift
@@ -10,8 +10,12 @@ import SwiftUI
 struct ContentView: View {
     @ObservedObject var vm = MainViewModel()
     var body: some View {
-        GeometryReader { canvas in
-            
+        GeometryReader { bounds in
+            VStack {
+                Spacer()
+                Canvas(defaultWidth: bounds.size.width)
+                Spacer()
+            }
         }
         .background(Color.blue)
         
