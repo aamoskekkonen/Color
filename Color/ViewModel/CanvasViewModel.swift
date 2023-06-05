771eba47646e539b8cfcd216586545f299bb7fb8
aamoskekkonen <aamoskekkonen@icloud.com>
2023-06-05 18:11:10 +0300

    refactor CanvasViewModel

--- Color/ViewModel/CanvasViewModel.swift
+++ Color/ViewModel/CanvasViewModel.swift
@@ -18,46 +18,16 @@ class CanvasViewModel: ObservableObject {
     @Published var lastClickedColor: OklchColor?
     
     let defaultPointDiameter: CGFloat
-    
-    static var referenceColors: [OklchColor] {
-        var colors: [OklchColor] = []
-        for l in stride(from: 0.0, through: 100.0, by: 10.0) {
-            var neutralAdded = false
-            for c in stride(from: 0.0, through: OklchColor.maxA, by: 0.1) {
-                for h in stride(from: 0.0, through: 350.0, by: 10.0) {
-                    let color = OklchColor(l: l, c: c, h: CGFloat(h))
-                    if c == 0.0 && h == 0 {
-                        colors.append(color)
-                        neutralAdded = true
-                    }
-                    if !(c == 0.0 && neutralAdded) {
-                        colors.append(color)
-                    }
-                }
-            }
-        }
-        return colors
-    }
-    
-    
-    
-    var origo: CGPoint {
-        return CGPoint(x: defaultCanvasWidth / 2, y: defaultCanvasWidth / 2)
-    }
 
     init(colors: [OklchColor], initialCanvasWidth: CGFloat, initialPointDiameter: CGFloat) {
         self.defaultCanvasWidth = initialCanvasWidth
         self.currentCanvasWidth = initialCanvasWidth
         self.data = colors.map { color in
-            //print("The color is \(color.name): (l = \(color.l), a = \(color.a), b = \(color.b), c = \(color.c), h = \(color.h)")
             let radius = initialCanvasWidth / 2
             let aRatio = color.a / OklchColor.maxA
             let bRatio = color.b / OklchColor.maxB
-            //print("aRatio = \(aRatio), bRatio = \(bRatio)")
             let x = radius + aRatio * radius
             let y = radius - bRatio * radius
-            //print("(x,y) = \((x, y))")
-            //print("")
             return ColorRepresentationData(
                 color: color,
                 point: CGPoint(x: x, y: y),
@@ -90,4 +60,31 @@ struct ColorRepresentationData: Identifiable {
     let point: CGPoint
     let diameter: CGFloat
     var isSelected: Bool
+}
+
+extension CanvasViewModel {
+    
+    var origo: CGPoint {
+        return CGPoint(x: defaultCanvasWidth / 2, y: defaultCanvasWidth / 2)
+    }
+    
+    static var referenceColors: [OklchColor] {
+        var colors: [OklchColor] = []
+        for l in stride(from: 0.0, through: 100.0, by: 10.0) {
+            var neutralAdded = false
+            for c in stride(from: 0.0, through: OklchColor.maxA, by: 0.1) {
+                for h in stride(from: 0.0, through: 350.0, by: 10.0) {
+                    let color = OklchColor(l: l, c: c, h: CGFloat(h))
+                    if c == 0.0 && h == 0 {
+                        colors.append(color)
+                        neutralAdded = true
+                    }
+                    if !(c == 0.0 && neutralAdded) {
+                        colors.append(color)
+                    }
+                }
+            }
+        }
+        return colors
+    }
 }
